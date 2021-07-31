e7eba125cff9ebfaafa995767f1080cfde87db67
godhiraj-code <dhirajdas.666@gmail.com>
2021-08-01 03:22:52 +0800

    few python lines to check the internet speed test

---
+++ checkinternetspeed.py
@@ -0,0 +1,9 @@
+import speedtest
+
+speed = speedtest.Speedtest()
+download_speed = speed.download()
+upload_speed = speed.upload()
+
+print(f"The download speed is : {download_speed}")
+print(f"The upload speed is : {upload_speed}")
+
